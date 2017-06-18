c31d459ea5ddd211256472647485e87d1b16a5d8
Benjamin van Zwienen <benjaminvanzwienen@live.nl>
2017-06-18 17:27:40 +0200

    Make /public a static asset folder for express

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public/index.html"));
 });
