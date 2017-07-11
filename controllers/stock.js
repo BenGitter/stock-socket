906012d74ab3d8154163dc3e4c2b9983f6d22074
Benjamin van Zwienen <benjaminvanzwienen@live.nl>
2017-07-11 19:58:43 +0200

    Add addQuote method

--- controllers/stock.js
+++ controllers/stock.js
@@ -5,6 +5,9 @@ const request = require("request");
 require("dotenv").load();
 const apiKey = process.env.APIKEY;
 
+// Array with quotes
+let quotes = [];
+
 exports.getQuoteData = function(quote, callback){
   request(`http://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=${quote}&apikey=${apiKey}`, (err, data) => {
     if(JSON.parse(data.body)["Error Message"]){
@@ -13,5 +16,21 @@ exports.getQuoteData = function(quote, callback){
       return callback(null, JSON.parse(data.body));
     }
   }); 
+}
+
+exports.addQuote = function(quote, callback){
+  const _quote = quote.toLowerCase();
+  if(quotes.indexOf(_quote) >= 0){
+    return callback(null, "Quote already displayed");
+  }else{
+    getQuoteData(_quote, (err, data) => {
+      if(err){
+        return callback("Quote doesn't exist", null);
+      }else{
+        quotes.push(_quote);
+        return callback(null, "Quote added");
+      }
+    });
+  }
 }
   
