281d0ad2b6ff4c6d2686ab5fbd1cebe17d62cd41
Benjamin van Zwienen <benjaminvanzwienen@live.nl>
2017-07-11 15:03:21 +0200

    Add some styling + add chart

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ChartsModule } from 'ng2-charts/ng2-charts';
+
 import { AppComponent } from './app.component';
+import { GraphComponent } from './graph/graph.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GraphComponent,
+    SidebarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ angular-src/src/app/graph/graph.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-graph',
+  templateUrl: './graph.component.html',
+  styleUrls: ['./graph.component.css']
+})
+export class GraphComponent implements OnInit {
+
+  chartDatasets = [{
+    label: "X",
+    data: [1,2,3]
+  }];
+  chartLabels = ["a", "b", "c"];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
