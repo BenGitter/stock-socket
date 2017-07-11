4f058213ce5b96fa58d7dc9b60ec3cffcef1164f
Benjamin van Zwienen <benjaminvanzwienen@live.nl>
2017-07-11 19:49:05 +0200

    Change from monthly to weekly quote data

--- controllers/stock.js
+++ controllers/stock.js
@@ -6,8 +6,7 @@ require("dotenv").load();
 const apiKey = process.env.APIKEY;
 
 exports.getQuoteData = function(quote, callback){
-  console.log(`http://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=${quote}&apikey=${apiKey}`);
-  request(`http://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=${quote}&apikey=${apiKey}`, (err, data) => {
+  request(`http://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=${quote}&apikey=${apiKey}`, (err, data) => {
     if(JSON.parse(data.body)["Error Message"]){
       return callback(JSON.parse(data.body)["Error Message"], null);
     }else{
