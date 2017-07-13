d6c9b461055fceedd23bddf58c54a803ecbc3c0f
Benjamin van Zwienen <benjaminvanzwienen@live.nl>
2017-07-13 15:27:03 +0200

    Get simple graph to work

--- angular-src/src/app/graph/graph.component.ts
+++ angular-src/src/app/graph/graph.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import * as Chart from 'chart.js';
+
+import { SocketService } from '../socket.service';
 
 @Component({
   selector: 'app-graph',
@@ -6,20 +10,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./graph.component.css']
 })
 export class GraphComponent implements OnInit {
+  newQuotesSub:Subscription;
+  chart:Chart;
 
-  chartDatasets = [{
-    label: "X",
-    data: [1,2,3]
-  }];
-  chartLabels = ["a", "b", "c"];
-  chartOptions = {
-    responsive: true,
-    maintainAspectRatio: false
-  };
-
-  constructor() { }
+  constructor(private socketService:SocketService) { }
 
   ngOnInit() {
+    // Chart.js 
+    const canvas = <HTMLCanvasElement>document.getElementById("chart");
+    const ctx = canvas.getContext("2d");
+    this.chart = new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: ["red", "blue", "yellow"],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3],
+          borderColor: this.getRandomColor(),
+          fill: false
+        }]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          yAxes: [{
+            ticks: {
+              // beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
+
+    // Test updating chart
+    setTimeout(() => {
+      const color = this.getRandomColor();
+      this.chart.data.datasets.push({label: "Test", data:[1,2,3], borderColor: color, fill: false});
+      this.chart.update();
+    }, 1000);
+
+    // Subscribe to "new quote" event
+    this.newQuotesSub = this.socketService.getNewQuotes().subscribe(quote => {
+      console.log(quote);
+    });
+  }
+
+
+  getRandomColor(){
+    const r = Math.random()*255;
+    const g = Math.random()*255;
+    const b = Math.random()*255;
+
+    return `rgb(${r},${g},${b})`;
   }
 
 }
